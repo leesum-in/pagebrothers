3594802ad7e94f90ca4769b9d42bf32c3fc44af7
Eun Oh <bdohhhhh@gmail.com>
2024-09-08 14:05:14 +0900

    [chore]
    packages/config-eslint/next.js 에서 parserOptions, settings 사용 안함으로 처리(주석처리)

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -1,7 +1,7 @@
-import './globals.css';
 import '@repo/shared/styles.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
