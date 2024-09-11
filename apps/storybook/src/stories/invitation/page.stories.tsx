4eba179e851d20128600dd1ce30f166a66216087
YIS <dtdt203@naver.com>
2024-09-11 20:27:11 +0900

    [add] storybook 구조
    - stories 폴더안에 stories.tsx 파일들 모아서 관리

---
+++ apps/storybook/src/stories/invitation/page.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Page from '../../../../invitation/src/app/page';
+
+export default {
+  title: 'invitation/Page',
+  component: Page,
+};
+
+export const Default = () => <Page />;
---
+++ apps/storybook/src/stories/www/page.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Page from '../../../../www/src/app/page';
+
+export default {
+  title: 'www/Page',
+  component: Page,
+};
+
+export const Default = () => <Page />;
