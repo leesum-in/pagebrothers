5d89713cb5298116b22707297ac1ac7745772b6a
Eun Oh <bdohhhhh@gmail.com>
2024-09-12 13:06:55 +0900

    [script]
    www 디렉토리 내에서 @ 임포트 사용할 수 있도록 next.config.js 및 tsconfig 설정

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ['@repo/shared'],
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src'); // '@/...' -> 'src/...'
+    return config;
+  },
 };
