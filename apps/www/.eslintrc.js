8303cc9e7831032c508d741c7839b593967a6f3f
Eun Oh <bdohhhhh@gmail.com>
2024-09-12 13:37:13 +0900

    [chore]
    eslint 의 개행 권고 사항 적용

--- apps/www/.eslintrc.js
+++ apps/www/.eslintrc.js
@@ -1,4 +1,5 @@
 const { resolve } = require('node:path');
+
 const project = resolve(__dirname, 'tsconfig.json');
 
 module.exports = {
--- apps/www/src/providers/Auth.provider.tsx
+++ apps/www/src/providers/Auth.provider.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { type PropsWithChildren } from 'react';
+
 import { AuthContext } from '../contexts';
 
 export function AuthProvider({ children }: PropsWithChildren): React.ReactNode {
