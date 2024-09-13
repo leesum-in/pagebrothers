545d36053c64e91e0d3d320e02ad9e213286b8ca
Jiyoung <jiyoung990602@gmail.com>
2024-09-13 20:34:19 +0900

    fix : apps/storybook Button.stories.ts Story 수정

--- apps/storybook/src/stories/Button.stories.ts
+++ apps/storybook/src/stories/Button.stories.ts
@@ -24,29 +24,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const Default: Story = {
   args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
+    backgroundColor: 'bg-[#aaa]',
     label: 'Button',
   },
 };
