ac63c08065e63bd1c9214eb46f477c310641d1ce
Jiyoung <jiyoung990602@gmail.com>
2024-09-13 20:26:26 +0900

    test : Button.tsx tailwind-css test code 작성

--- apps/storybook/src/stories/Button.tsx
+++ apps/storybook/src/stories/Button.tsx
@@ -1,52 +1,18 @@
-import React from 'react';
-import './button.css';
-
 export interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
   onClick?: () => void;
+  backgroundColor: string;
 }
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({ label, onClick, backgroundColor, ...props }: ButtonProps) => {
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={`${backgroundColor} w-full h-[48px] px-[16px] rounded-md`}
+      onClick={onClick}
       {...props}
     >
       {label}
-      <style jsx>{`
-        button {
-          background-color: ${backgroundColor};
-        }
-      `}</style>
     </button>
   );
 };
