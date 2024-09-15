643e263ca8bcbaaf82cbca082fc53e9b41f7cf0c
Eun Oh <bdohhhhh@gmail.com>
2024-09-15 17:39:22 +0900

    [update]: Auth.type.ts 에 에러 타입 추가

--- apps/www/src/types/Auth.type.ts
+++ apps/www/src/types/Auth.type.ts
@@ -3,3 +3,10 @@ export enum SocialLoginType {
   NAVER = 'NAVER',
   GOOGLE = 'GOOGLE',
 }
+
+export type SocialLoginError = {
+  message: string;
+  type: string;
+  code: number;
+  trace?: string;
+};
