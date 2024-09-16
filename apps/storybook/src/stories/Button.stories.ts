ba976e1a1bafc65157377f97d9eb4578318c37da
Jiyoung <jiyoung990602@gmail.com>
2024-09-16 13:16:34 +0900

    style
    기본 UI 설정 및 props 수정

--- apps/storybook/src/stories/Button.stories.ts
+++ apps/storybook/src/stories/Button.stories.ts
@@ -14,7 +14,7 @@ const meta = {
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    colorType: { control: 'color' },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
@@ -24,9 +24,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Default: Story = {
+export const Primary: Story = {
   args: {
-    backgroundColor: 'bg-[#aaa]',
-    label: 'Button',
+    colorType: 'primary',
+    label: '적용하기',
   },
 };
--- packages/shared/src/components/Button.tsx
+++ packages/shared/src/components/Button.tsx
@@ -1,14 +1,18 @@
 export interface ButtonProps {
   label: string;
   onClick?: () => void;
-  backgroundColor: string;
+  colorType: string;
 }
 
-export const Button = ({ label, onClick, backgroundColor, ...props }: ButtonProps) => {
+const BUTTON_COLOR: { [key: string]: string } = {
+  primary: 'bg-[#4F46E5] text-[#fff]',
+};
+
+export const Button = ({ label, onClick, colorType, ...props }: ButtonProps) => {
   return (
     <button
       type="button"
-      className={`${backgroundColor} w-full h-[48px] px-[16px] rounded-md`}
+      className={`${BUTTON_COLOR[colorType]} w-full h-[48px] px-[16px] rounded-md text-[14px] font-bold box-border drop-shadow-md`}
       onClick={onClick}
       {...props}
     >
