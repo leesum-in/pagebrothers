865cb3fd87e018c8f2560fcbdfb1840c675c06a3
Eun Oh <bdohhhhh@gmail.com>
2024-09-16 20:17:45 +0900

    [update]: LogIn 컴포넌트 수정
    AuthWrapper 를 사용하도록 수정

--- apps/www/src/components/organisms/auth/LogIn.tsx
+++ apps/www/src/components/organisms/auth/LogIn.tsx
@@ -1,25 +1,21 @@
-'use client';
-
-import LogInHeader from '@/components/atoms/auth/LogInHeader';
 import SocialLoginButton from '@/components/molecules/auth/SocialLoginButton';
 import { SocialLoginType } from '@/types';
 
+import AuthWrapper from './AuthWrapper';
+
 function LogIn(): React.ReactNode {
   return (
-    <main className="h-dvh grid place-items-center px-8 py-16">
-      <section className="w-full desktop:max-w-sm">
-        <LogInHeader />
-        <div className="mt-8">
-          <ul className="space-y-2 font-bold">
-            {Object.values(SocialLoginType).map((type) => (
-              <li key={type}>
-                <SocialLoginButton type={type} />
-              </li>
-            ))}
-          </ul>
-        </div>
-      </section>
-    </main>
+    <AuthWrapper type="login">
+      <div className="mt-8">
+        <ul className="space-y-2 font-bold">
+          {Object.values(SocialLoginType).map((type) => (
+            <li key={type}>
+              <SocialLoginButton type={type} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </AuthWrapper>
   );
 }
 
