685f6cf570ce34a11d97688a53f4b397229a8e01
Eun Oh <bdohhhhh@gmail.com>
2024-09-16 20:18:57 +0900

    [update]: AuthProvider 업데이트 중
    일단 카카오 로그인 시도 중

--- apps/www/src/providers/Auth.provider.tsx
+++ apps/www/src/providers/Auth.provider.tsx
@@ -1,12 +1,26 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { type PropsWithChildren } from 'react';
 
 import { AuthContext } from '../contexts';
 
 export function AuthProvider({ children }: PropsWithChildren): React.ReactNode {
+  const router = useRouter();
+
+  const logInWithKakao = (): void => {
+    const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+    // const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
+    const API_URL = process.env.NEXT_PUBLIC_API_URL;
+    // const REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
+    // const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${API_URL}${REDIRECT_URI}?&scope=profile_nickname,account_email,profile_image`;
+    const kakaoAuthUrl = `${API_URL}/oauth2/authorize/kakao?redirect_uri=${BASE_URL}/login/callback?backUrl=${BASE_URL}/start&register_uri=${BASE_URL}/register?backUrl=${BASE_URL}/start`;
+    router.push(kakaoAuthUrl);
+  };
+
   const value = {
     me: null,
+    logInWithKakao,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
