636637ab720813950b6fef0bb2bc8541f68b5e3e
Jiyoung <jiyoung990602@gmail.com>
2024-09-16 12:52:36 +0900

    del : Button.tsx 관련 코드 삭제

--- apps/storybook/src/stories/Header.tsx
+++ apps/storybook/src/stories/Header.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-
-import { Button } from './Button';
 import './header.css';
 
 type User = {
@@ -42,13 +39,9 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps
             <span className="welcome">
               Welcome, <b>{user.name}</b>!
             </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
           </>
         ) : (
-          <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
-          </>
+          <></>
         )}
       </div>
     </div>
