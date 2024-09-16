57ab0b236fbc4bf5081afa25406fbdf25c9808ae
Eun Oh <bdohhhhh@gmail.com>
2024-09-16 20:18:37 +0900

    [update]: AuthProvider 업데이트 중
    일단 카카오 로그인 시도 중

--- apps/www/src/contexts/auth.context.ts
+++ apps/www/src/contexts/auth.context.ts
@@ -4,10 +4,12 @@ import { createContext } from 'react';
 
 export interface AuthContextInterface {
   me: null;
+  logInWithKakao: () => void;
 }
 
 const initialValue: AuthContextInterface = {
   me: null,
+  logInWithKakao: () => {},
 };
 
 export const AuthContext = createContext<AuthContextInterface>(initialValue);
