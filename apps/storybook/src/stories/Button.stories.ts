0dc8f0ebcbea727dedb58555e0e7b1c8b8c9d358
Jiyoung <jiyoung990602@gmail.com>
2024-09-16 14:21:54 +0900

    [ style ]
    Button variants 속성 및 story 추가

--- apps/storybook/src/stories/Button.stories.ts
+++ apps/storybook/src/stories/Button.stories.ts
@@ -14,7 +14,7 @@ const meta = {
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    colorType: { control: 'color' },
+    variants: { control: 'color' },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
@@ -26,7 +26,42 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    colorType: 'primary',
+    variants: 'primary',
     label: '적용하기',
+  },
+};
+
+export const Default: Story = {
+  args: {
+    variants: 'default',
+    label: '미리보기',
+  },
+};
+
+export const White: Story = {
+  args: {
+    variants: 'white',
+    label: '미리보기',
+  },
+};
+
+export const Dashed: Story = {
+  args: {
+    variants: 'dashed',
+    label: '위젯 추가하기',
+  },
+};
+
+export const TextPrimary: Story = {
+  args: {
+    variants: 'textPrimary',
+    label: '편집',
+  },
+};
+
+export const TextDefault: Story = {
+  args: {
+    variants: 'textDefault',
+    label: '삭제',
   },
 };
--- packages/shared/src/components/Button.tsx
+++ packages/shared/src/components/Button.tsx
@@ -1,18 +1,23 @@
 export interface ButtonProps {
   label: string;
   onClick?: () => void;
-  colorType: string;
+  variants: string;
 }
 
 const BUTTON_COLOR: { [key: string]: string } = {
-  primary: 'bg-[#4F46E5] text-[#fff]',
+  primary: 'bg-[#4F46E5] text-[#fff] drop-shadow-md',
+  default: 'bg-[#F1F5F9] border border-solid border-[#E2E8F0] text-[#0F172A] drop-shadow-md',
+  white: 'bg-[#fff] border border-solid border-[#E2E8F0] text-[#0F172A] drop-shadow-md',
+  dashed: 'bg-transparent border border-dashed border-[#CBD5E1] text-[#475569]',
+  textPrimary: 'bg-transparent text-[#4F46E5]',
+  textDefault: 'bg-transparent text-black',
 };
 
-export const Button = ({ label, onClick, colorType, ...props }: ButtonProps) => {
+export const Button = ({ label, onClick, variants, ...props }: ButtonProps) => {
   return (
     <button
       type="button"
-      className={`${BUTTON_COLOR[colorType]} w-full h-[48px] px-[16px] rounded-md text-[14px] font-bold box-border drop-shadow-md`}
+      className={`${BUTTON_COLOR[variants]} w-full h-[48px] px-[16px] rounded-md text-[14px] font-bold box-border`}
       onClick={onClick}
       {...props}
     >
