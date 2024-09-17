e269a44e34cbd392dc486300f04f20fc91d73f8b
Eun Oh <bdohhhhh@gmail.com>
2024-09-17 17:15:00 +0900

    [update]: useMeQuery 훅 수정
    반환 값이 null 이 될 수도 있도록 타입 정의 수정

--- apps/www/src/hooks/queries/me.query.ts
+++ apps/www/src/hooks/queries/me.query.ts
@@ -5,8 +5,8 @@ import { getMeFromClient } from '@/apis/auth/me-client.api';
 import { QUERY_KEY_ME } from '@/constants';
 import type { MeResponse, SocialLoginError } from '@/types';
 
-export function useMeQuery(): UseQueryResult<MeResponse, SocialLoginError> {
-  return useQuery<MeResponse, SocialLoginError>({
+export function useMeQuery(): UseQueryResult<MeResponse | null, SocialLoginError> {
+  return useQuery<MeResponse | null, SocialLoginError>({
     queryKey: [QUERY_KEY_ME],
     queryFn: getMeFromClient,
   });
