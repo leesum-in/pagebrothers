d0a9752ff42ff431466faf9fccecb0b7f1af914f
Eun Oh <bdohhhhh@gmail.com>
2024-09-17 17:17:29 +0900

    [update]: Auth.provider.tsx 수정
    useMeQuery 추가(현재는 클라이언트에서는 작동하지 않고 서버에서 프리페치한 값만 initial로 넘어오는 상태), useEffect로 error 및 pending 상태 추적

--- apps/www/src/providers/Auth.provider.tsx
+++ apps/www/src/providers/Auth.provider.tsx
@@ -1,26 +1,43 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { type PropsWithChildren } from 'react';
+import { useEffect, type PropsWithChildren } from 'react';
+
+import { useMeQuery } from '@/hooks/queries';
 
 import { AuthContext } from '../contexts';
 
 export function AuthProvider({ children }: PropsWithChildren): React.ReactNode {
   const router = useRouter();
+  const { data: me, isPending, error } = useMeQuery();
 
-  const logInWithKakao = (): void => {
+  // 처음부터 만들기 << 버튼으로 이동시 backUrl 추가 https://dev.pagesisters.cc/start
+  const logInStartWithKakao = (): void => {
     const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
     // const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
     const API_URL = process.env.NEXT_PUBLIC_API_URL;
     // const REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
     // const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${API_URL}${REDIRECT_URI}?&scope=profile_nickname,account_email,profile_image`;
-    const kakaoAuthUrl = `${API_URL}/oauth2/authorize/kakao?redirect_uri=${BASE_URL}/login/callback?backUrl=${BASE_URL}/start&register_uri=${BASE_URL}/register?backUrl=${BASE_URL}/start`;
+    const kakaoAuthUrl = `${API_URL}/oauth2/authorize/kakao?redirect_uri=${BASE_URL}/login/callback?backUrl=/start&register_uri=${BASE_URL}/register?backUrl=/`;
     router.push(kakaoAuthUrl);
   };
 
+  useEffect(() => {
+    console.log('loading');
+  }, [isPending]);
+
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  // 나중에 삭제할 임시 코드 me 있는지 추적하기 위해
+  useEffect(() => {
+    console.log('me =====>', me);
+  }, [me]);
+
   const value = {
-    me: null,
-    logInWithKakao,
+    me: me ?? null,
+    logInStartWithKakao,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
