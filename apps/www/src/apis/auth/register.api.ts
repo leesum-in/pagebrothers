aefaa9e88d7f73b71d2d9f470ba224822620ec32
Eun Oh <bdohhhhh@gmail.com>
2024-09-17 17:05:38 +0900

    [update]: register api 함수 수정
    응답 헤더에서 토큰을 추출하고 로컬스토리지에 저장하는 로직 삭제

--- apps/www/src/apis/auth/register.api.ts
+++ apps/www/src/apis/auth/register.api.ts
@@ -1,21 +1,14 @@
-import { API_URL } from '@/constants';
-import type { RegisterData, SocialLoginResponse } from '@/types';
+import type { RegisterData, SocialLoginError, SocialLoginResponse } from '@/types';
+import { fetchWrapper } from '@/utils';
 
 export async function postRegister(registerData: RegisterData): Promise<SocialLoginResponse> {
   const url = '/auth/register';
-  try {
-    const response = await fetch(`${API_URL}${url}`, {
-      method: 'POST',
-      body: JSON.stringify(registerData),
-    });
-
-    // 응답 헤더에서 토큰 추출
-    const token = response.headers.get('Authorization');
-    if (token) localStorage.setItem('token', token);
-
-    const data = (await response.json()) as SocialLoginResponse;
-    return data;
-  } catch (error) {
-    throw new Error('회원가입 실패');
-  }
+  const data = await fetchWrapper<SocialLoginResponse, SocialLoginError>(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(registerData),
+  });
+  return data;
 }
