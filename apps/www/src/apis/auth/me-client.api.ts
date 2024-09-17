72f7d35ad8a5ec74127f7ad54614646c3b28987c
Eun Oh <bdohhhhh@gmail.com>
2024-09-17 17:06:58 +0900

    [update]: getMe 서버, 클라이언트 api 함수 수정
    서버의 경우 token을 인자로 받아서 전송할 수 있도록 수정, 클라이언트의 경우 로컬스토리지에 토큰 저장 안하는 쪽으로 변경되서 쓸지 안쓸지 모르겠음

--- apps/www/src/apis/auth/me-client.api.ts
+++ apps/www/src/apis/auth/me-client.api.ts
@@ -1,8 +1,15 @@
 import type { MeResponse, SocialLoginError } from '@/types';
 import { fetchWrapper } from '@/utils';
 
-export async function getMeFromClient(): Promise<MeResponse> {
+export async function getMeFromClient(): Promise<MeResponse | null> {
   const url = '/user/me';
+  const token = localStorage.getItem('pagebrothers-token');
+
+  if (!token) {
+    return null;
+  }
+
+  // 토큰을 로컬 스토리지에 저장하지 않는데 이게 쓸모가 있을까?
   const data = await fetchWrapper<MeResponse, SocialLoginError>(url, {
     method: 'GET',
     headers: {
--- apps/www/src/apis/auth/me-server.api.ts
+++ apps/www/src/apis/auth/me-server.api.ts
@@ -1,12 +1,19 @@
 import type { MeResponse, SocialLoginError } from '@/types';
 import { fetchWrapper } from '@/utils';
 
-export async function getMeFromServer(): Promise<MeResponse> {
+interface GetMeFromServerProps {
+  token: string | null;
+}
+
+export async function getMeFromServer({ token }: GetMeFromServerProps): Promise<MeResponse | null> {
   const url = '/user/me';
+  if (!token) {
+    return null;
+  }
   const data = await fetchWrapper<MeResponse, SocialLoginError>(url, {
     method: 'GET',
     headers: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      Authorization: `Bearer ${token}`,
     },
   });
   return data;
