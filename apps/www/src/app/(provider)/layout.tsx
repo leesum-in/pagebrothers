f4cc38c5f9d49994d6508f57a23acfe058d84083
Eun Oh <bdohhhhh@gmail.com>
2024-09-17 17:10:41 +0900

    [update]: (provider)/layout 에 프리페치 쿼리 추가
    login/callback 을 거치면 쿠키에 토큰이 써지고 이 값을 사용해 서버사이드에서 me객체를 prefetchQuery 하고, dehydrate 합니다.

--- apps/www/src/app/(provider)/layout.tsx
+++ apps/www/src/app/(provider)/layout.tsx
@@ -1,9 +1,29 @@
-import { type PropsWithChildren } from 'react';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { cookies } from 'next/headers';
+import { Suspense, type PropsWithChildren } from 'react';
 
+import { getMeFromServer } from '@/apis/auth/me-server.api';
+import { QUERY_KEY_ME } from '@/constants';
 import { AuthProvider } from '@/providers';
 
-function ProviderLayout({ children }: PropsWithChildren): React.ReactNode {
-  return <AuthProvider>{children}</AuthProvider>;
+async function ProviderLayout({ children }: PropsWithChildren): Promise<React.ReactNode> {
+  const cookieStore = cookies();
+  const token = cookieStore.get('pagebrothers-token')?.value;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: [QUERY_KEY_ME],
+    queryFn: () => getMeFromServer({ token: token ?? null }),
+  });
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <HydrationBoundary state={dehydratedState}>
+        <AuthProvider>{children}</AuthProvider>
+      </HydrationBoundary>
+    </Suspense>
+  );
 }
 
 export default ProviderLayout;
