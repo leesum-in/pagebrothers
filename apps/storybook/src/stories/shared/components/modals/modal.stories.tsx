d2d3528b6b412814cbaa89cdad0a4c9c955ba672
YIS <dtdt203@naver.com>
2024-09-20 02:13:10 +0900

    [fix] 중첩모달되는지만 테스트

--- apps/storybook/src/stories/shared/components/modals/modal.stories.tsx
+++ apps/storybook/src/stories/shared/components/modals/modal.stories.tsx
@@ -56,4 +56,76 @@ addModal.args = {
   modalHeader: <h2>에드 모달 헤더</h2>,
   modalFooter: <h2>에드 모달 푸터</h2>,
   children: <div>에드 모달 내용</div>,
-}; //요런식
+}; //테스트 하고싶은 모달 이런식으로 추가
+
+//중첩모달 테스트
+const MultiModalTemplate: StoryFn = (args) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMultiModalOpen, setIMsultiModalOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        onClick={() => setIsModalOpen(true)}
+        className="border border-gray-300 rounded-lg px-4 py-2 hover:bg-gray-100"
+      >
+        모달창 열기
+      </button>
+      <Modal
+        isModalOpen={isModalOpen}
+        onCloseModal={() => setIsModalOpen(false)}
+        isModalHeader={args.isModalHeader}
+        isModalFooter={args.isModalFooter}
+        modalHeader={args.modalHeader}
+        modalFooter={args.modalFooter}
+        zIndex={50}
+        scale={1}
+      >
+        {args.children}
+        <button
+          onClick={() => setIMsultiModalOpen(true)}
+          className="mt-4 border border-gray-300 rounded-lg px-4 py-2 hover:bg-gray-100"
+        >
+          중첩 모달 열기
+        </button>
+      </Modal>
+      <Modal
+        isModalOpen={isMultiModalOpen}
+        onCloseModal={() => setIMsultiModalOpen(false)}
+        isModalHeader={args.IsMultiModalHeader}
+        isModalFooter={args.IsMultiModalFooter}
+        modalHeader={args.MultiModalHeader}
+        modalFooter={args.MultiModalFooter}
+        zIndex={60}
+        scale={0.75}
+      >
+        {args.MultiModalChildren}
+      </Modal>
+    </>
+  );
+};
+
+export const MultiModal = MultiModalTemplate.bind({});
+MultiModal.args = {
+  isModalHeader: true,
+  isModalFooter: true,
+  modalHeader: <h2>첫 번째 모달 헤더</h2>,
+  modalFooter: <h2>첫 번째 모달 푸터</h2>,
+  children: (
+    <div>
+      <div className=" bg-gray-100 px-10 py-20 ">첫 번째 모달 내용</div>
+      <div className=" bg-gray-100 px-10 py-10 mt-6">추가 내용</div>
+    </div>
+  ),
+
+  IsMultiModalHeader: true,
+  IsMultiModalFooter: true,
+  MultiModalHeader: <h2>중첩 모달 헤더</h2>,
+  MultiModalFooter: <h2>중첩 모달 푸터</h2>,
+  MultiModalChildren: (
+    <div>
+      <div className=" bg-gray-100 px-10 py-20 ">중첩 모달 내용1</div>
+      <div className=" bg-gray-100 px-10 py-20 mt-6">중첩 모달 내용2</div>
+    </div>
+  ),
+};
--- packages/shared/src/components/modals/Modal.tsx
+++ packages/shared/src/components/modals/Modal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 interface ModalProps {
   isModalOpen: boolean;
@@ -8,6 +9,8 @@ interface ModalProps {
   modalFooter?: React.ReactNode;
   isModalHeader?: boolean;
   isModalFooter?: boolean;
+  zIndex?: number;
+  scale?: number;
 }
 
 const Modal: React.FC<ModalProps> = ({
@@ -18,25 +21,27 @@ const Modal: React.FC<ModalProps> = ({
   isModalFooter,
   modalHeader,
   modalFooter,
+  zIndex = 50,
+  scale = 1,
 }) => {
   // 모달 배경
-  const modalBackground = `fixed inset-0 bg-blue-700 bg-opacity-30 transition-opacity duration-300 z-40 ${
+  const modalBackground = `fixed inset-0 bg-blue-700 bg-opacity-30 transition-opacity duration-300 z-[${zIndex - 1} ${
     isModalOpen ? 'opacity-50' : 'opacity-0 pointer-events-none'
   }`;
 
   // 모달 컨테이너
-  const modalContainer = `fixed bottom-0 left-0 right-0 md:bottom-0 left-0 right-0 top-0 flex items-end md:items-center justify-center z-50 transform transition-all duration-300 ${
+  const modalContainer = `fixed bottom-0 left-0 right-0 items-end justify-center md:bottom-0 left-0 right-0 top-0 flex md:items-center  z-[${zIndex}]  transform transition-all duration-300 ${
     isModalOpen ? 'pointer-events-auto backdrop-blur-sm' : 'pointer-events-none'
   }`;
 
   // 모달 내용
   const modalContent = `relative bg-white w-full md:w-[480px] max-h-[90vh] rounded-t-2xl md:rounded-2xl shadow-lg flex flex-col overflow-y-auto transform transition-all duration-300 ${
-    isModalOpen ? 'opacity-100 translate-y-0 ' : 'opacity-0 translate-y-full md:translate-y-4 '
-  }`;
+    isModalOpen ? `opacity-100 translate-y-0 ` : 'opacity-0 translate-y-full md:translate-y-4 '
+  }scale-${scale === 0.75 ? '75' : '100'}`;
 
-  return (
+  return ReactDOM.createPortal(
     <>
-      <div className={modalBackground} onClick={onCloseModal}></div>
+      <div className={modalBackground}></div>
       <div className={modalContainer} onClick={onCloseModal}>
         <div className={modalContent} onClick={(e) => e.stopPropagation()}>
           {isModalHeader && (
@@ -48,7 +53,8 @@ const Modal: React.FC<ModalProps> = ({
           )}
         </div>
       </div>
-    </>
+    </>,
+    document.body,
   );
 };
 
