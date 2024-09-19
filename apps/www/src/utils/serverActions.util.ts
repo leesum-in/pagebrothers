2d09059f6bbf853aa0b18e0b5f83192aea9649e6
Eun Oh <bdohhhhh@gmail.com>
2024-09-19 15:02:44 +0900

    [update]: 빌드 에러 해결? await 가 반드시 필요하다네요?

--- apps/www/src/utils/serverActions.util.ts
+++ apps/www/src/utils/serverActions.util.ts
@@ -4,6 +4,6 @@ import { cookies } from 'next/headers';
 
 export async function deleteCookie(name: string): Promise<void> {
   const cookieStore = cookies();
-  cookieStore.delete(name);
-  cookieStore.set(name, '', { maxAge: 0 });
+  await cookieStore.delete(name);
+  await cookieStore.set(name, '', { maxAge: 0 });
 }
