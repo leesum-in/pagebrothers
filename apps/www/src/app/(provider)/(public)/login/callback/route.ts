34c77f8c07e0cc43e116a4bb6227950cd12fb96a
Eun Oh <bdohhhhh@gmail.com>
2024-09-19 14:59:54 +0900

    [update]: 빌드 에러 해결
    route.ts 에서는 "void | Response | Promise<void | Response>" 이렇게만 리턴해야 한다고 하네요...

--- apps/www/src/app/(provider)/(public)/login/callback/route.ts
+++ apps/www/src/app/(provider)/(public)/login/callback/route.ts
@@ -1,9 +1,8 @@
-import type { Redirect } from 'next';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { type NextRequest } from 'next/server';
 
-export function GET(request: NextRequest): Redirect {
+export function GET(request: NextRequest): void {
   const cookieStore = cookies();
   const searchParams = request.nextUrl.searchParams;
   const token = searchParams.get('token');
