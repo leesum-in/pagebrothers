215ad186c03daa3b34f6b4d8d2e86b9f08cfabfd
Eun Oh <bdohhhhh@gmail.com>
2024-09-20 02:58:06 +0900

    [update]: AuthWrapper 가로 폭 확보 되지 않아서 w-dvw 임시 추가

--- apps/www/src/auth/components/AuthWrapper.tsx
+++ apps/www/src/auth/components/AuthWrapper.tsx
@@ -8,7 +8,7 @@ interface AuthWrapperProps extends PropsWithChildren {
 
 function AuthWrapper({ children, type }: AuthWrapperProps): React.ReactNode {
   return (
-    <main className="h-dvh grid place-items-center px-8 py-16">
+    <main className="h-dvh w-dvw grid place-items-center px-8 py-16">
       <section className="w-full desktop:max-w-sm">
         <AuthHeader type={type} />
         {children}
