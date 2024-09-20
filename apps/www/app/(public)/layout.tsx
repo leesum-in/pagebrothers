603e37eadfd6aadd0deedfc2a9660b5923eaf25d
Eun Oh <bdohhhhh@gmail.com>
2024-09-20 15:18:32 +0900

    [update]: 빌드 에러 해결을 위해 (public)/layout 에 suspense 처리
    이 에러가 갑자기 왜 나는지 모르겠지만.. 참고 링크 https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout

--- apps/www/app/(public)/layout.tsx
+++ apps/www/app/(public)/layout.tsx
@@ -1,13 +1,13 @@
-import { type PropsWithChildren } from 'react';
+import { Suspense, type PropsWithChildren } from 'react';
 
 import Header from '@/common/components/Header';
 
 function PublicLayout({ children }: PropsWithChildren): React.ReactNode {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Header />
       {children}
-    </>
+    </Suspense>
   );
 }
 
