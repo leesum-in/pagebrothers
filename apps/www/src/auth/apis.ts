e05de4b473bba18fb38cfac5e0b519f5ea93164b
Eun Oh <bdohhhhh@gmail.com>
2024-09-20 15:09:38 +0900

    [update]: apis 에서 getMeFromServer 삭제, hooks 에서 logOut 함수 반환타입 수정

--- apps/www/src/auth/apis.ts
+++ apps/www/src/auth/apis.ts
@@ -1,5 +1,6 @@
-import type { Me, RegisterData, SocialLoginResponse } from '@/auth/types';
 import api from '@repo/shared/src/api';
+
+import type { Me, RegisterData, SocialLoginResponse } from '@/auth/types';
 
 export async function getMeFromClient(): Promise<Me | null> {
   const url = '/user/me';
@@ -8,24 +9,10 @@ export async function getMeFromClient(): Promise<Me | null> {
     return null;
   }
   api.defaults.headers.common.Authorization = `Bearer ${token}`;
-  return await api.get<Me, Me>(url);
-}
-
-interface GetMeFromServerProps {
-  token: string | null;
-}
-
-export async function getMeFromServer({ token }: GetMeFromServerProps): Promise<Me | null> {
-  const url = '/user/me';
-  if (!token) {
-    return null;
-  }
-  api.defaults.headers.common.Authorization = `Bearer ${token}`;
-  return await api.get<Me, Me>(url);
+  return api.get<Me, Me>(url);
 }
 
 export async function postRegister(registerData: RegisterData): Promise<SocialLoginResponse> {
   const url = '/auth/register';
-  const response = await api.post<SocialLoginResponse>(url, registerData);
-  return response.data;
+  return api.post<SocialLoginResponse, SocialLoginResponse>(url, registerData);
 }
