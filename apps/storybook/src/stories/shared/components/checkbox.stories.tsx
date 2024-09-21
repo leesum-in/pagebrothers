60a1134bdfa1b764cc8e7535519406a3f55feb82
KIM DOEHEE <kimamy20@gmail.com>
2024-09-22 04:51:50 +0900

    [add] : Checkbox storybook

---
+++ apps/storybook/src/stories/shared/components/checkbox.stories.tsx
@@ -0,0 +1,63 @@
+import { Meta, StoryFn } from '@storybook/react';
+import Checkbox from '../../../../../../packages/shared/src/components/Checkbox';
+
+export default {
+  title: 'Shared/Components/Checkbox',
+  component: Checkbox,
+  argTypes: {
+    label: {
+      control: { type: 'select', options: ['small', 'large', 'none'] },
+      description: 'Label size: small, large, none',
+    },
+    checked: {
+      control: 'boolean',
+      description: 'Checkbox 체크 여부',
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'Disables checkbox',
+    },
+    labelText: {
+      control: 'text',
+      description: 'Checkbox 설명 Text label',
+    },
+  },
+} as Meta<typeof Checkbox>;
+
+const Template: StoryFn<typeof Checkbox> = (args) => <Checkbox {...args} />;
+
+// Default Checkbox 스토리
+export const Default = Template.bind({});
+Default.args = {
+  label: 'small',
+  checked: false,
+  disabled: false,
+  labelText: '디폴트 레이블 입력',
+};
+
+// Checked Checkbox 스토리
+export const Checked = Template.bind({});
+Checked.args = {
+  label: 'small',
+  checked: true,
+  disabled: false,
+  labelText: '체크됨 레이블 입력',
+};
+
+// Disabled Checkbox 스토리
+export const Disabled = Template.bind({});
+Disabled.args = {
+  label: 'small',
+  checked: false,
+  disabled: true,
+  labelText: '비활성화 레이블 입력',
+};
+
+// Checked & Disabled Checkbox 스토리
+export const CheckedDisabled = Template.bind({});
+CheckedDisabled.args = {
+  label: 'small',
+  checked: true,
+  disabled: true,
+  labelText: '체크 & 비활성화됨 레이블 입력',
+};
