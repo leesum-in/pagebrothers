a381c6be3d9ffba7648222ba34a95e7aa65f3e4f
KIM DOEHEE <kimamy20@gmail.com>
2024-09-23 03:27:56 +0900

    [update] : 가독성과 코드 효율을 위해 중첩된 분기 처리 수정

--- packages/shared/src/components/Checkbox.tsx
+++ packages/shared/src/components/Checkbox.tsx
@@ -54,31 +54,13 @@ const Checkbox = ({
   };
 
   // default, disabled, checked & disabled 상태별 스타일
-  const checkboxStateStyles = disabled
-    ? checked
-      ? 'bg-indigo-100 border-none'
-      : 'bg-slate-50 border-[0.1rem] border-slate-100'
-    : checked
-      ? 'bg-indigo-500 border-none'
-      : 'border-[0.1rem] border-slate-200';
+  const checkboxStateStyles = getCheckboxStateStyles(enabled, disabled);
 
-  // 각 사이즈별 체크박스 스타일
-  const checkboxSizeStyles =
-    label === 'small'
-      ? 'w-[1.125rem] h-[1.125rem] border-[0.1rem] rounded-[0.25rem] gap-0'
-      : label === 'large'
-        ? 'w-[1.25rem] h-[1.25rem] border-[0.1rem] rounded-[0.25rem] gap-0'
-        : 'w-[1.5rem] h-[1.5rem] border-[0.1rem] rounded-[0.25rem] gap-0';
+  // 사이즈별 체크박스 스타일
+  const checkboxSizeStyles = getCheckboxSizeStyles(label);
 
-  // 각 사이즈별 텍스트 스타일
-  const labelSizeStyles =
-    label === 'small'
-      ? 'text-[0.875rem] font-normal leading-[1.4rem] tracking-[-0.02em] text-justify'
-      : label === 'large'
-        ? 'text-[1rem] font-normal leading-[1.6rem] tracking-[-0.02em] text-justify'
-        : 'hidden';
-
-  const labelOpacity = disabled ? 'opacity-50' : '';
+  // 사이즈별 텍스트 스타일
+  const labelSizeStyles = getLabelSizeStyles(label);
 
   return (
     <div className="flex items-center">
@@ -100,7 +82,9 @@ const Checkbox = ({
           <path d="M3 8L6 11L11 3.5" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
         </svg>
       </HeadlessUiCheckbox>
-      <span className={`ml-2 text-slate-600 ${labelSizeStyles} ${labelOpacity}`}>{labelText}</span>
+      <span className={`ml-2 text-slate-600 ${labelSizeStyles} ${disabled ? 'opacity-50' : ''}`}>
+        {labelText}
+      </span>
     </div>
   );
 };
