3d168b962332c833d66ab7e92238c34dbe46b424
KIM DOEHEE <kimamy20@gmail.com>
2024-09-23 03:46:15 +0900

    [update] : 길어진 로직(getToggleStyles와 getCircleStyles 함수) 가독성과 성능 최적화를 위해 컴포넌트 외부로 이동

--- packages/shared/src/components/Toggle.tsx
+++ packages/shared/src/components/Toggle.tsx
@@ -8,6 +8,34 @@ interface ToggleProps {
   labelText?: string;
 }
 
+// 토글 컨테이너 상태별 스타일
+const getToggleStyles = (disabled: boolean, enabled: boolean) => {
+  switch (true) {
+    case disabled && enabled:
+      return 'w-[3.25rem] h-[1.5rem] rounded-full bg-indigo-100 border border-slate-200';
+    case disabled:
+      return 'w-[3.25rem] h-[1.5rem] rounded-full bg-slate-100 border border-slate-200';
+    case enabled:
+      return 'w-[3.25rem] h-[1.5rem] rounded-full bg-indigo-500 border border-slate-200';
+    default:
+      return 'w-[3.25rem] h-[1.5rem] rounded-full bg-slate-100 border border-slate-200';
+  }
+};
+
+// 토글 버튼 상태별 스타일
+const getCircleStyles = (disabled: boolean, enabled: boolean) => {
+  switch (true) {
+    case disabled && enabled:
+      return 'w-[1.75rem] h-[1.75rem] rounded-full bg-slate-50 border border-slate-200 translate-x-[1.75rem]';
+    case disabled:
+      return 'w-[1.75rem] h-[1.75rem] rounded-full bg-slate-50 border border-slate-200 translate-x-[-0.25rem]';
+    case enabled:
+      return 'w-[1.75rem] h-[1.75rem] rounded-full bg-white border border-slate-200 shadow-[0_0_0.5rem_#0000000A,_-0.125rem_0_0.125rem_#0000001F] translate-x-[1.75rem]';
+    default:
+      return 'w-[1.75rem] h-[1.75rem] rounded-full bg-white border border-slate-200 shadow-[0_0_0.5rem_#0000000A,_0.125rem_0_0.125rem_#0000000A] translate-x-[-0.25rem]';
+  }
+};
+
 const Toggle = ({
   label = 'left',
   toggleOn = false,
@@ -16,33 +44,8 @@ const Toggle = ({
 }: ToggleProps) => {
   const [enabled, setEnabled] = useState(toggleOn);
 
-  // 토글 컨테이너 상태별 스타일
-  const getToggleStyles = (disabled: boolean, enabled: boolean) => {
-    switch (true) {
-      case disabled && enabled:
-        return 'w-[3.25rem] h-[1.5rem] rounded-full bg-indigo-100 border border-slate-200';
-      case disabled:
-        return 'w-[3.25rem] h-[1.5rem] rounded-full bg-slate-100 border border-slate-200';
-      case enabled:
-        return 'w-[3.25rem] h-[1.5rem] rounded-full bg-indigo-500 border border-slate-200';
-      default:
-        return 'w-[3.25rem] h-[1.5rem] rounded-full bg-slate-100 border border-slate-200';
-    }
-  };
-
-  // 토글 버튼 상태별 스타일
-  const getCircleStyles = (disabled: boolean, enabled: boolean) => {
-    switch (true) {
-      case disabled && enabled:
-        return 'w-[1.75rem] h-[1.75rem] rounded-full bg-slate-50 border border-slate-200 translate-x-[1.75rem]';
-      case disabled:
-        return 'w-[1.75rem] h-[1.75rem] rounded-full bg-slate-50 border border-slate-200 translate-x-[-0.25rem]';
-      case enabled:
-        return 'w-[1.75rem] h-[1.75rem] rounded-full bg-white border border-slate-200 shadow-[0_0_0.5rem_#0000000A,_-0.125rem_0_0.125rem_#0000001F] translate-x-[1.75rem]';
-      default:
-        return 'w-[1.75rem] h-[1.75rem] rounded-full bg-white border border-slate-200 shadow-[0_0_0.5rem_#0000000A,_0.125rem_0_0.125rem_#0000000A] translate-x-[-0.25rem]';
-    }
-  };
+  const toggleStyles = getToggleStyles(disabled, enabled);
+  const circleStyles = getCircleStyles(disabled, enabled);
 
   return (
     <Switch.Group as="div" className="flex items-center">
@@ -57,12 +60,10 @@ const Toggle = ({
         checked={enabled}
         onChange={setEnabled}
         disabled={disabled}
-        className={`relative inline-flex items-center justify-start ${getToggleStyles()} ${
-          disabled ? 'cursor-not-allowed' : 'cursor-pointer'
-        }`}
+        className={`relative inline-flex items-center justify-start ${toggleStyles} ${disabled ? 'cursor-not-allowed' : 'cursor-pointer'}`}
       >
         <span
-          className={`absolute top-1/2 transform -translate-y-1/2 transition-transform ${getCircleStyles()}`}
+          className={`absolute top-1/2 transform -translate-y-1/2 transition-transform ${circleStyles}`}
         />
       </Switch>
       {label === 'right' && labelText && (
