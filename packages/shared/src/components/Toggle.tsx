8981da11992f52ec60d8917fd9b13e89d5f1e173
KIM DOEHEE <kimamy20@gmail.com>
2024-09-23 03:44:05 +0900

    [update] : 가독성을 위해 분기 처리 수정

--- packages/shared/src/components/Toggle.tsx
+++ packages/shared/src/components/Toggle.tsx
@@ -17,31 +17,31 @@ const Toggle = ({
   const [enabled, setEnabled] = useState(toggleOn);
 
   // 토글 컨테이너 상태별 스타일
-  const getToggleStyles = () => {
-    if (disabled && enabled) {
-      return 'w-[3.25rem] h-[1.5rem] rounded-full bg-indigo-100 border border-slate-200';
+  const getToggleStyles = (disabled: boolean, enabled: boolean) => {
+    switch (true) {
+      case disabled && enabled:
+        return 'w-[3.25rem] h-[1.5rem] rounded-full bg-indigo-100 border border-slate-200';
+      case disabled:
+        return 'w-[3.25rem] h-[1.5rem] rounded-full bg-slate-100 border border-slate-200';
+      case enabled:
+        return 'w-[3.25rem] h-[1.5rem] rounded-full bg-indigo-500 border border-slate-200';
+      default:
+        return 'w-[3.25rem] h-[1.5rem] rounded-full bg-slate-100 border border-slate-200';
     }
-    if (disabled) {
-      return 'w-[3.25rem] h-[1.5rem] rounded-full bg-slate-100 border border-slate-200';
-    }
-    if (enabled) {
-      return 'w-[3.25rem] h-[1.5rem] rounded-full bg-indigo-500 border border-slate-200';
-    }
-    return 'w-[3.25rem] h-[1.5rem] rounded-full bg-slate-100 border border-slate-200';
   };
 
   // 토글 버튼 상태별 스타일
-  const getCircleStyles = () => {
-    if (disabled && enabled) {
-      return 'w-[1.75rem] h-[1.75rem] rounded-full bg-slate-50 border border-slate-200 translate-x-[1.75rem]';
+  const getCircleStyles = (disabled: boolean, enabled: boolean) => {
+    switch (true) {
+      case disabled && enabled:
+        return 'w-[1.75rem] h-[1.75rem] rounded-full bg-slate-50 border border-slate-200 translate-x-[1.75rem]';
+      case disabled:
+        return 'w-[1.75rem] h-[1.75rem] rounded-full bg-slate-50 border border-slate-200 translate-x-[-0.25rem]';
+      case enabled:
+        return 'w-[1.75rem] h-[1.75rem] rounded-full bg-white border border-slate-200 shadow-[0_0_0.5rem_#0000000A,_-0.125rem_0_0.125rem_#0000001F] translate-x-[1.75rem]';
+      default:
+        return 'w-[1.75rem] h-[1.75rem] rounded-full bg-white border border-slate-200 shadow-[0_0_0.5rem_#0000000A,_0.125rem_0_0.125rem_#0000000A] translate-x-[-0.25rem]';
     }
-    if (disabled) {
-      return 'w-[1.75rem] h-[1.75rem] rounded-full bg-slate-50 border border-slate-200 translate-x-[-0.25rem]';
-    }
-    if (enabled) {
-      return 'w-[1.75rem] h-[1.75rem] rounded-full bg-white border border-slate-200 shadow-[0_0_0.5rem_#0000000A,_-0.125rem_0_0.125rem_#0000001F] translate-x-[1.75rem]';
-    }
-    return 'w-[1.75rem] h-[1.75rem] rounded-full bg-white border border-slate-200 shadow-[0_0_0.5rem_#0000000A,_0.125rem_0_0.125rem_#0000000A] translate-x-[-0.25rem]';
   };
 
   return (
