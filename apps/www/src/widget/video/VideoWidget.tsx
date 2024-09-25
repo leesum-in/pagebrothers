f0e53ababb264bd32f853e7d8be773489949718b
Eun Oh <bdohhhhh@gmail.com>
2024-09-26 08:45:51 +0900

    [update]: test > videoWidget 작성 중

--- apps/www/src/widget/video/VideoWidget.tsx
+++ apps/www/src/widget/video/VideoWidget.tsx
@@ -3,7 +3,14 @@ import WidgetWrapper from '@/widget/WidgetWrapper';
 function VideoWidget(): React.ReactNode {
   return (
     <WidgetWrapper title="Video">
-      <div>VideoWidget</div>
+      <div style={{ paddingTop: '56.25%' }} />
+      <iframe
+        className="absolute inset-0 w-full h-full"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      />
     </WidgetWrapper>
   );
 }
