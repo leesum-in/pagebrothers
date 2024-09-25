af06a1691d4459fe67ca7747bb6b5f2ab495d09e
Eun Oh <bdohhhhh@gmail.com>
2024-09-25 17:48:48 +0900

    [add]: test > 비디오 위젯 작성 시작

---
+++ apps/www/src/widget/video/VideoWidget.tsx
@@ -0,0 +1,11 @@
+import WidgetWrapper from '@/widget/WidgetWrapper';
+
+function VideoWidget(): React.ReactNode {
+  return (
+    <WidgetWrapper title="Video">
+      <div>VideoWidget</div>
+    </WidgetWrapper>
+  );
+}
+
+export default VideoWidget;
