61b49c309edd1c594e6e39acb8dfb8c51c16771e
Eun Oh <bdohhhhh@gmail.com>
2024-09-27 11:37:59 +0900

    [chore]: eslint 설정 업데이트

--- packages/config-eslint/react.js
+++ packages/config-eslint/react.js
@@ -37,6 +37,10 @@ module.exports = {
   // add rules configurations here
   rules: {
     'import/no-default-export': 'off',
+    'unicorn/filename-case': 'off', // 파일 이름 규칙 끄기
+    'react/jsx-sort-props': 'off', // 컴포넌트 props 정렬 끄기
+    '@typescript-eslint/explicit-function-return-type': 'off', // 함수 반환 타입 끄기
+    '@typescript-eslint/prefer-string-starts-ends-with': 'off', // 문자열 startsWith, endsWith 로 바꾸라는 경고 끄기
   },
   overrides: [
     {
