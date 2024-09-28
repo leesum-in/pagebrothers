ad5c2e416b068a50feaca33fd7aa8c586b5a1e32
Eun Oh <bdohhhhh@gmail.com>
2024-09-28 13:00:15 +0900

    [update]: widget button 수정중

--- apps/www/src/widget/WidgetModifyButtons.tsx
+++ apps/www/src/widget/WidgetModifyButtons.tsx
@@ -1,15 +1,19 @@
+import { Button } from '@repo/shared';
+
 import WidgetStickerButton from './WidgetStickerButton';
 
 function WidgetModifyButtons(): React.ReactNode {
+  // 이렇게 쓰면 되나요..?
   return (
     <div className="flex gap-2">
       <WidgetStickerButton />
-      <button
-        type="button"
-        className=" h-8 rounded-sm px-2 text-xs border border-slate-200 bg-slate-100 text-slate-900 hover:bg-slate-200 center-flex gap-2 font-bold shadow-1 transition-colors disabled:opacity-40"
+      <Button
+        variants="fill_white"
+        size="small"
+        className="bg-slate-100 hover:bg-slate-200 transition-colors"
       >
         위젯 수정
-      </button>
+      </Button>
     </div>
   );
 }
--- apps/www/src/widget/WidgetTitleButton.tsx
+++ apps/www/src/widget/WidgetTitleButton.tsx
@@ -5,6 +5,8 @@ interface WidgetTitleButtonProps {
 }
 
 function WidgetTitleButton({ title }: WidgetTitleButtonProps): React.ReactNode {
+  // 시맨틱태그를 위한 button 등은 shared 컴포넌트를 안쓰는 것인가...?
+  // 디렉토리 구조를 widget/video/common 이런식으로 하는게 좋을까??
   return (
     <button type="button" className="center-flex h-8 touch-none gap-3">
       <Move className="text-xl text-slate-500" />
