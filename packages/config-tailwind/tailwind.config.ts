f8a2fe9685f85b6b6e2c6673a8f78a49dab61598
Eun Oh <bdohhhhh@gmail.com>
2024-09-28 13:33:52 +0900

    [update]: tailwind sharedConfig 설정 업데이트

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -6,116 +6,120 @@ type FontStyleValue = [string, { lineHeight: string; letterSpacing: string; font
 // We want each package to be responsible for its own content.
 const config: Omit<Config, 'content'> = {
   theme: {
-    fontFamily: {
-      sans: [
-        'var(--font-pretendard)',
-        'ui-sans-serif',
-        'system-ui',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        "'Segoe UI'",
-        'Roboto',
-        "'Helvetica Neue'",
-        'Arial',
-        "'Noto Sans'",
-        'sans-serif',
-        "'Apple Color Emoji'",
-        "'Segoe UI Emoji'",
-        "'Segoe UI Symbol'",
-        "'Noto Color Emoji'",
-      ],
-      serif: [
-        'var(--font-gowunBatang)',
-        'var(--font-notoSerifKr)',
-        'ui-sans-serif',
-        'system-ui',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        "'Segoe UI'",
-        'Roboto',
-        "'Helvetica Neue'",
-        'Arial',
-        "'Noto Sans'",
-        'sans-serif',
-        "'Apple Color Emoji'",
-        "'Segoe UI Emoji'",
-        "'Segoe UI Symbol'",
-        "'Noto Color Emoji'",
-      ],
-      noto: [
-        'var(--font-notoSerifKr)',
-        'serif',
-        'ui-sans-serif',
-        'system-ui',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        "'Segoe UI'",
-        'Roboto',
-        "'Helvetica Neue'",
-        'Arial',
-        "'Noto Sans'",
-        'sans-serif',
-        "'Apple Color Emoji'",
-        "'Segoe UI Emoji'",
-        "'Segoe UI Symbol'",
-        "'Noto Color Emoji'",
-      ],
-    },
-    fontWeight: {
-      thin: '100',
-      extralight: '200',
-      light: '300',
-      normal: '400',
-      medium: '500',
-      semibold: '600',
-      bold: '700',
-      extrabold: '800',
-      black: '900',
-    },
-    fontSize: {
-      // Head styles
-      // h - 1.5
-      h1x: ['48px', { lineHeight: '65.28px', letterSpacing: '-0.03em', fontWeight: '900' }],
-      h2x: ['42px', { lineHeight: '60.48px', letterSpacing: '-0.03em', fontWeight: '900' }],
-      h3x: ['36px', { lineHeight: '51.84px', letterSpacing: '-0.03em', fontWeight: '900' }],
-      // h - black
-      h1b: ['32px', { lineHeight: '43.52px', letterSpacing: '-0.02em', fontWeight: '900' }],
-      h2b: ['28px', { lineHeight: '40.32px', letterSpacing: '-0.02em', fontWeight: '900' }],
-      h3b: ['24px', { lineHeight: '34.56px', letterSpacing: '-0.02em', fontWeight: '900' }],
-      // h - extra bold
-      h4b: ['20px', { lineHeight: '32px', letterSpacing: '-0.02em', fontWeight: '800' }],
-      h5b: ['18px', { lineHeight: '28.8px', letterSpacing: '-0.02em', fontWeight: '800' }],
-      // h - regular
-      h1: ['32px', { lineHeight: '43.52px', letterSpacing: '-0.02em', fontWeight: '400' }],
-      h2: ['28px', { lineHeight: '40.32px', letterSpacing: '-0.02em', fontWeight: '400' }],
-      h3: ['24px', { lineHeight: '34.56px', letterSpacing: '-0.02em', fontWeight: '400' }],
-      h4: ['20px', { lineHeight: '32px', letterSpacing: '-0.02em', fontWeight: '400' }],
-      h5: ['18px', { lineHeight: '28.8px', letterSpacing: '-0.02em', fontWeight: '400' }],
-      // paragraph styles
-      // p - bold
-      p1b: ['16px', { lineHeight: '25.6px', letterSpacing: '-0.02em', fontWeight: '700' }],
-      p2b: ['14px', { lineHeight: '22.4px', letterSpacing: '-0.02em', fontWeight: '700' }],
-      // p - regular
-      p1: ['16px', { lineHeight: '25.6px', letterSpacing: '-0.02em', fontWeight: '400' }],
-      p2: ['14px', { lineHeight: '22.4px', letterSpacing: '-0.02em', fontWeight: '400' }],
-      // Caption styles
-      // c - bold
-      cb: ['12px', { lineHeight: '19.2px', letterSpacing: '-0.02em', fontWeight: '700' }],
-      cbe: ['12px', { lineHeight: '19.2px', letterSpacing: '0.04em', fontWeight: '700' }],
-      // c - regular
-      c: ['12px', { lineHeight: '19.2px', letterSpacing: '-0.02em', fontWeight: '400' }],
-      ce: ['12px', { lineHeight: '19.2px', letterSpacing: '0.04em', fontWeight: '400' }],
-    },
-    backgroundImage: {
-      gradient: 'linear-gradient(90deg, #4338CA 0%, #EF4444 100%)',
-      'gradient-white-bot2top': 'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #FFFFFF 100%)',
-      'gradient-slate-bot2top': 'linear-gradient(180deg, rgba(248, 250, 252, 0) 0%, #F8FAFC 100%)',
-    },
-    screens: {
-      desktop: '820px',
-    },
-    maxWidth: {
-      sm: '24rem',
+    extend: {
+      fontFamily: {
+        sans: [
+          'var(--font-pretendard)',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          "'Segoe UI'",
+          'Roboto',
+          "'Helvetica Neue'",
+          'Arial',
+          "'Noto Sans'",
+          'sans-serif',
+          "'Apple Color Emoji'",
+          "'Segoe UI Emoji'",
+          "'Segoe UI Symbol'",
+          "'Noto Color Emoji'",
+        ],
+        serif: [
+          'var(--font-gowunBatang)',
+          'var(--font-notoSerifKr)',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          "'Segoe UI'",
+          'Roboto',
+          "'Helvetica Neue'",
+          'Arial',
+          "'Noto Sans'",
+          'sans-serif',
+          "'Apple Color Emoji'",
+          "'Segoe UI Emoji'",
+          "'Segoe UI Symbol'",
+          "'Noto Color Emoji'",
+        ],
+        noto: [
+          'var(--font-notoSerifKr)',
+          'serif',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          "'Segoe UI'",
+          'Roboto',
+          "'Helvetica Neue'",
+          'Arial',
+          "'Noto Sans'",
+          'sans-serif',
+          "'Apple Color Emoji'",
+          "'Segoe UI Emoji'",
+          "'Segoe UI Symbol'",
+          "'Noto Color Emoji'",
+        ],
+      },
+      fontWeight: {
+        thin: '100',
+        extralight: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
+      },
+      fontSize: {
+        // Head styles
+        // h - 1.5
+        h1x: ['48px', { lineHeight: '65.28px', letterSpacing: '-0.03em', fontWeight: '900' }],
+        h2x: ['42px', { lineHeight: '60.48px', letterSpacing: '-0.03em', fontWeight: '900' }],
+        h3x: ['36px', { lineHeight: '51.84px', letterSpacing: '-0.03em', fontWeight: '900' }],
+        // h - black
+        h1b: ['32px', { lineHeight: '43.52px', letterSpacing: '-0.02em', fontWeight: '900' }],
+        h2b: ['28px', { lineHeight: '40.32px', letterSpacing: '-0.02em', fontWeight: '900' }],
+        h3b: ['24px', { lineHeight: '34.56px', letterSpacing: '-0.02em', fontWeight: '900' }],
+        // h - extra bold
+        h4b: ['20px', { lineHeight: '32px', letterSpacing: '-0.02em', fontWeight: '800' }],
+        h5b: ['18px', { lineHeight: '28.8px', letterSpacing: '-0.02em', fontWeight: '800' }],
+        // h - regular
+        h1: ['32px', { lineHeight: '43.52px', letterSpacing: '-0.02em', fontWeight: '400' }],
+        h2: ['28px', { lineHeight: '40.32px', letterSpacing: '-0.02em', fontWeight: '400' }],
+        h3: ['24px', { lineHeight: '34.56px', letterSpacing: '-0.02em', fontWeight: '400' }],
+        h4: ['20px', { lineHeight: '32px', letterSpacing: '-0.02em', fontWeight: '400' }],
+        h5: ['18px', { lineHeight: '28.8px', letterSpacing: '-0.02em', fontWeight: '400' }],
+        // paragraph styles
+        // p - bold
+        p1b: ['16px', { lineHeight: '25.6px', letterSpacing: '-0.02em', fontWeight: '700' }],
+        p2b: ['14px', { lineHeight: '22.4px', letterSpacing: '-0.02em', fontWeight: '700' }],
+        // p - regular
+        p1: ['16px', { lineHeight: '25.6px', letterSpacing: '-0.02em', fontWeight: '400' }],
+        p2: ['14px', { lineHeight: '22.4px', letterSpacing: '-0.02em', fontWeight: '400' }],
+        // Caption styles
+        // c - bold
+        cb: ['12px', { lineHeight: '19.2px', letterSpacing: '-0.02em', fontWeight: '700' }],
+        cbe: ['12px', { lineHeight: '19.2px', letterSpacing: '0.04em', fontWeight: '700' }],
+        // c - regular
+        c: ['12px', { lineHeight: '19.2px', letterSpacing: '-0.02em', fontWeight: '400' }],
+        ce: ['12px', { lineHeight: '19.2px', letterSpacing: '0.04em', fontWeight: '400' }],
+      },
+      backgroundImage: {
+        gradient: 'linear-gradient(90deg, #4338CA 0%, #EF4444 100%)',
+        'gradient-white-bot2top':
+          'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #FFFFFF 100%)',
+        'gradient-slate-bot2top':
+          'linear-gradient(180deg, rgba(248, 250, 252, 0) 0%, #F8FAFC 100%)',
+      },
+      screens: {
+        desktop: '820px',
+      },
+      maxWidth: {
+        sm: '24rem',
+      },
     },
   },
   plugins: [
@@ -133,7 +137,23 @@ const config: Omit<Config, 'content'> = {
           };
         }
       });
+      const customUtilities: Record<string, Record<string, string>> = {
+        '.center-flex': {
+          display: 'flex',
+          'justify-content': 'center',
+          'align-items': 'center',
+        },
+        '.no-interaction': {
+          'pointer-events': 'none',
+          'touch-action': 'none',
+          '-webkit-user-select': 'none',
+          '-moz-user-select': 'none',
+          '-ms-user-select': 'none',
+          'user-select': 'none',
+        },
+      };
       addUtilities(fontUtilities);
+      addUtilities(customUtilities);
     }),
   ],
 };
