a2cd3f1e074d57556852ec34a19f929029592530
Eun Oh <bdohhhhh@gmail.com>
2024-10-01 19:27:22 +0900

    [add]: index 추가

---
+++ apps/www/src/ui/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from './Header';
+export { default as HeaderDropDownMenu } from './HeaderDropDownMenu';
+export { default as HeaderMenu } from './HeaderMenu';
+export { default as Navigation } from './Navigation';
