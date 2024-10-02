11aeb67c5747a6b10f713c6fd8387a4c9b466f86
KIM DOEHEE <kimamy20@gmail.com>
2024-10-02 12:46:10 +0900

    [update] : tailwind class 관리를 위해 cn 함수와 className props 추가

--- packages/shared/src/components/elements/textInput/TextInput.tsx
+++ packages/shared/src/components/elements/textInput/TextInput.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, useState } from 'react';
+import { cn } from 'src';
 
 interface TextInputProps {
   error?: boolean;
@@ -12,6 +13,7 @@ interface TextInputProps {
   placeholder?: string;
   value: string;
   onChange: (value: string) => void;
+  className?: string;
 }
 
 const TextInput = ({
@@ -25,16 +27,18 @@ const TextInput = ({
   placeholder = '텍스트 인풋',
   value,
   onChange,
+  className,
 }: TextInputProps) => {
   const [focused, setFocused] = useState(false);
 
-  const inputClassName = `
-    block w-full rounded pl-[3rem] pr-[3.5rem] border text-p1 text-slate-600 leading-6 caret-indigo-700 h-[3rem]
-    ${error ? 'border-red-500' : 'border-slate-200'}
-    ${disabled ? 'cursor-not-allowed bg-gray-100' : ''}
-    ${focused && !disabled ? 'outline-2 outline-indigo-700 border-indigo-700' : ''}
-    hover:shadow-[0_4px_12px_0_rgba(19,32,57,0.1),_0_8px_20px_0_rgba(19,32,57,0.03)]
-  `;
+  const inputClassName = cn(
+    'block w-full rounded pl-[3rem] pr-[3.5rem] border text-p1 text-slate-600 leading-6 caret-indigo-700 h-[3rem]',
+    error ? 'border-red-500' : 'border-slate-200',
+    disabled ? 'cursor-not-allowed bg-gray-100' : '',
+    focused && !disabled ? 'outline-2 outline-indigo-700 border-indigo-700' : '',
+    'hover:shadow-[0_4px_12px_0_rgba(19,32,57,0.1),_0_8px_20px_0_rgba(19,32,57,0.03)]',
+    className,
+  );
 
   return (
     <div className="w-full max-w-md px-4">
