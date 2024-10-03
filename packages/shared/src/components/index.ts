c71cda6d9969390a93e047098974831ac23c8729
Jiyoung <jiyoung990602@gmail.com>
2024-10-03 15:28:00 +0900

    [fix]
    card export 경로 삭제

--- packages/shared/src/components/index.ts
+++ packages/shared/src/components/index.ts
@@ -1,8 +1,6 @@
 export { Button } from './button';
-export { Card } from './card';
-export * from './modal';
 export { Checkbox } from './checkbox';
 export { LongTextField } from './longTextField';
+export * from './modal';
 export { Radio } from './radio';
 export { Toggle } from './toggle';
-
