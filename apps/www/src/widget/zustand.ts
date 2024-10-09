5b85de23abe36696e241136af23637018653dbcf
Eun Oh <bdohhhhh@gmail.com>
2024-10-10 08:30:57 +0900

    [update]: zustand 업데이트

--- apps/www/src/widget/zustand.ts
+++ apps/www/src/widget/zustand.ts
@@ -1,28 +1,49 @@
+import type { SubmitHandler } from 'react-hook-form';
 import { create } from 'zustand';
 
-import type { WidgetType } from '@/types/pageBrothers.type';
+import type { WidgetItem } from '@/types/pageBrothers.type';
+
+export type VideoWidgetForm = {
+  url: string;
+  aspectWidth: number;
+  aspectHeight: number;
+};
+
+export type SubmitHandlers = VideoWidgetForm;
 
 export type ModalStore = {
   modalState: {
     isOpen: boolean;
-    type: WidgetType | null;
+    widget: WidgetItem | null;
   };
-  openModal: (type: WidgetType) => void;
+  invitationId: string;
+  onSubmit: SubmitHandler<SubmitHandlers>;
+  openModal: (type: WidgetItem) => void;
   closeModal: () => void;
+  setOnSubmit: (onSubmit: SubmitHandler<SubmitHandlers>) => void;
+  setInvitationId: (invitationId: string) => void;
 };
 
 const useModalStore = create<ModalStore>((set) => ({
   modalState: {
     isOpen: false,
-    type: null,
+    widget: null,
   },
-  openModal: (type: WidgetType) => {
-    set((state: ModalStore) => ({ modalState: { ...state.modalState, isOpen: true, type } }));
+  invitationId: '',
+  onSubmit: () => {},
+  openModal: (widget: WidgetItem) => {
+    set((state: ModalStore) => ({ modalState: { ...state.modalState, isOpen: true, widget } }));
   },
   closeModal: () => {
     set((state: ModalStore) => ({
-      modalState: { ...state.modalState, isOpen: false, type: null },
+      modalState: { ...state.modalState, isOpen: false, widget: null },
     }));
+  },
+  setOnSubmit: (onSubmit: SubmitHandler<SubmitHandlers>) => {
+    set((state: ModalStore) => ({ ...state, onSubmit }));
+  },
+  setInvitationId: (invitationId: string) => {
+    set((state: ModalStore) => ({ ...state, invitationId }));
   },
 }));
 
