2d0b47001d08ca304ca134b918684ff31c3597f7
Eun Oh <bdohhhhh@gmail.com>
2024-10-09 23:49:36 +0900

    [update]: 템플릿에 Modal 컴포넌트 넣고 WidgetModifyButton 에서 openModal

--- apps/www/src/invitations/dashboard/components/EditTemplate.tsx
+++ apps/www/src/invitations/dashboard/components/EditTemplate.tsx
@@ -1,17 +1,26 @@
 'use client';
 
+import { Modal } from '@repo/shared';
 import { useParams } from 'next/navigation';
 import { useEffect } from 'react';
+import { useShallow } from 'zustand/shallow';
 
 import { useInvitationQuery } from '@/invitations/queries';
 import ErrorTemplate from '@/ui/error/ErrorTemplate';
 import { PageWrapper } from '@/ui/wrapper';
-import { Widget } from '@/widget/common';
+import { Widget, WidgetModal, WidgetModalFooter, WidgetModalHeader } from '@/widget/common';
+import type { ModalStore } from '@/widget/zustand';
+import useModalStore from '@/widget/zustand';
 
 function EditTemplate() {
   const { id } = useParams<{ id: string }>();
   const { data: invitation, isPending, error } = useInvitationQuery(id);
-
+  const { modalState, closeModal } = useModalStore(
+    useShallow((state: ModalStore) => ({
+      modalState: state.modalState,
+      closeModal: state.closeModal,
+    })),
+  );
   useEffect(() => {
     if (invitation) console.log('invitation ====>', invitation);
   }, [invitation]);
@@ -26,22 +35,37 @@ function EditTemplate() {
   }
 
   return (
-    <PageWrapper>
-      <div className="desktop:flex-1">
-        <div className="mx-auto w-full max-w-[26rem]">
-          <div className="space-y-6">
-            {/** 아래 그냥 예시입니다 */}
-            {invitation?.widgets[0] ? (
-              <Widget invitation={invitation} widgetItem={invitation.widgets[0]} />
-            ) : null}
-            {invitation?.widgets[1] ? (
-              <Widget invitation={invitation} widgetItem={invitation.widgets[1]} />
-            ) : null}
+    <>
+      <Modal
+        isModalOpen={modalState.isOpen}
+        onCloseModal={closeModal}
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log('submit');
+        }}
+        modalHeader={<WidgetModalHeader type={modalState.type} />}
+        modalFooter={<WidgetModalFooter />}
+      >
+        <WidgetModal type={modalState.type} />
+      </Modal>
+
+      <PageWrapper>
+        <div className="desktop:flex-1">
+          <div className="mx-auto w-full max-w-[26rem]">
+            <div className="space-y-6">
+              {/** 아래 그냥 예시입니다 */}
+              {invitation?.widgets[0] ? (
+                <Widget invitation={invitation} widgetItem={invitation.widgets[0]} />
+              ) : null}
+              {invitation?.widgets[1] ? (
+                <Widget invitation={invitation} widgetItem={invitation.widgets[1]} />
+              ) : null}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="mx-auto w-full max-w-[26rem] desktop:max-w-[22.5rem] desktop:flex-none desktop:self-start sticky top-[5.5rem] hidden desktop:block" />
-    </PageWrapper>
+        <div className="mx-auto w-full max-w-[26rem] desktop:max-w-[22.5rem] desktop:flex-none desktop:self-start sticky top-[5.5rem] hidden desktop:block" />
+      </PageWrapper>
+    </>
   );
 }
 
--- apps/www/src/widget/common/WidgetModifyButton.tsx
+++ apps/www/src/widget/common/WidgetModifyButton.tsx
@@ -1,11 +1,28 @@
+'use client';
+
 import { Button } from '@repo/shared';
 
-function WidgetModifyButton(): React.ReactNode {
+import type { WidgetType } from '@/types/pageBrothers.type';
+
+import useModalStore from '../zustand';
+
+interface WidgetModifyButtonProps {
+  type: WidgetType;
+}
+
+function WidgetModifyButton({ type }: WidgetModifyButtonProps): React.ReactNode {
+  const { openModal } = useModalStore();
+
+  const handleButtonClick = () => {
+    openModal(type);
+  };
+
   return (
     <Button
       variants="fill_white"
       size="small"
       className="bg-slate-100 hover:bg-slate-200 transition-colors drop-shadow-none"
+      onClick={handleButtonClick}
     >
       위젯 수정
     </Button>
