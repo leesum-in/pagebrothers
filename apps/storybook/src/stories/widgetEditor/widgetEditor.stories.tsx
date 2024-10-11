2d2d844f2da21aaa87c1329921099fb20400beca
KIM DOEHEE <kimamy20@gmail.com>
2024-10-11 17:12:31 +0900

    [add] : 위젯 편집 페이지 스토리북

---
+++ apps/storybook/src/stories/widgetEditor/widgetEditor.stories.tsx
@@ -0,0 +1,12 @@
+// import { Meta, StoryFn } from '@storybook/react';
+// import WidgetEditor from '../../../../www/src/invitations/dashboard/components/WidgetEditor';
+
+// export default {
+//   title: 'Components/WidgetEditor',
+//   component: WidgetEditor,
+// } as Meta;
+
+// const Template: StoryFn<typeof WidgetEditor> = (args) => <WidgetEditor {...args} />;
+
+// export const Default = Template.bind({});
+// Default.args = {};
