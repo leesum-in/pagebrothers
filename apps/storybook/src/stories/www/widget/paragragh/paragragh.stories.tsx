4e87b95cdc9caacbe90405d76362dd58d3716352
Jiyoung <jiyoung990602@gmail.com>
2024-10-15 20:38:23 +0900

    add
    paragragh.stories.tsx 파일 생성 및 경로 재설정

---
+++ apps/storybook/src/stories/www/widget/paragragh/paragragh.stories.tsx
@@ -0,0 +1,8 @@
+import { ParagraghWidget } from '@www/widget/paragragh';
+
+export default {
+  title: 'www/ParagraghWidget',
+  component: ParagraghWidget,
+};
+
+export const Default = () => <ParagraghWidget></ParagraghWidget>;
