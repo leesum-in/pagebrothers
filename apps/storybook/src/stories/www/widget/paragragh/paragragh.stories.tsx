44473edf00811866539260d7623bf982a57c29f5
Jiyoung <jiyoung990602@gmail.com>
2024-10-22 10:37:16 +0900

    [del]
    paragragh.stories 파일 삭제

--- apps/storybook/src/stories/www/widget/paragragh/paragragh.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { ParagraghWidget } from '@www/widget/paragragh';
-
-export default {
-  title: 'www/ParagraghWidget',
-  component: ParagraghWidget,
-};
-
-export const Default = () => <ParagraghWidget></ParagraghWidget>;
