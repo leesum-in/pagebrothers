6b5728fd73a2b52d8473da6714cd2ed5a324cfef
Jiyoung <jiyoung990602@gmail.com>
2024-10-22 22:17:15 +0900

    [style]
    타이틀 및 메세지 입력 시 style 추가

--- apps/www/src/widget/message/MessageWidget.tsx
+++ apps/www/src/widget/message/MessageWidget.tsx
@@ -1,4 +1,4 @@
-import type { IInvitation, WidgetItem } from '@/types/pageBrothers.type';
+import type { IInvitation, MessageWidgetConfig, WidgetItem } from '@/types/pageBrothers.type';
 import { WidgetWrapper } from '../common';
 
 interface MessageWidgetProps {
@@ -7,11 +7,22 @@ interface MessageWidgetProps {
 }
 
 function MessageWidget({ widgetItem }: MessageWidgetProps): React.ReactNode {
+  const { widgetTitle, title, align, size } = widgetItem.config as MessageWidgetConfig;
+
   return (
     <WidgetWrapper widgetItem={widgetItem}>
-      <div className="relative no-interaction py-20 px-4 text-center text-sm">
-        <p>메세지를 등록해주세요.</p>
-      </div>
+      {title ? (
+        <div className="no-interaction space-y-6 p-8 text-center text-base text-slate-700/70">
+          {widgetTitle && <p className="text-[1.12em] font-black"> {widgetTitle}</p>}
+          <div className="space-y-3">
+            <p>{title}</p>
+          </div>
+        </div>
+      ) : (
+        <div className="relative no-interaction py-20 px-4 text-center text-sm">
+          <p>메세지를 등록해주세요.</p>
+        </div>
+      )}
     </WidgetWrapper>
   );
 }
