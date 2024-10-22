c1e97a5f05f6645c2c01566d979555ea2a1c0e89
Jiyoung <jiyoung990602@gmail.com>
2024-10-22 21:39:03 +0900

    [style]
    입력값이 없을 경우 default 로 적용되는 UI 작업

--- apps/www/src/widget/message/MessageWidget.tsx
+++ apps/www/src/widget/message/MessageWidget.tsx
@@ -1,5 +1,19 @@
-function MessageWidget() {
-  return <div>MessageWidget</div>;
+import type { IInvitation, WidgetItem } from '@/types/pageBrothers.type';
+import { WidgetWrapper } from '../common';
+
+interface MessageWidgetProps {
+  invitation?: IInvitation;
+  widgetItem: WidgetItem;
+}
+
+function MessageWidget({ widgetItem }: MessageWidgetProps): React.ReactNode {
+  return (
+    <WidgetWrapper widgetItem={widgetItem}>
+      <div className="relative no-interaction py-20 px-4 text-center text-sm">
+        <p>메세지를 등록해주세요.</p>
+      </div>
+    </WidgetWrapper>
+  );
 }
 
 export default MessageWidget;
