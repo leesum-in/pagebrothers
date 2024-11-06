7f8d8452da90f8f3e81255bda35a968f1c512ead
Eun Oh <bdohhhhh@gmail.com>
2024-11-06 21:51:05 +0900

    [chore]

--- apps/www/src/widgets/components/WidgetWrapper.tsx
+++ apps/www/src/widgets/components/WidgetWrapper.tsx
@@ -4,7 +4,7 @@ import { type PropsWithChildren } from 'react';
 import WidgetButtons from './WidgetButtons';
 
 interface WidgetWrapperProps {
-  widgetItem: WidgetItem;
+  widgetItem?: WidgetItem;
   isMultiModal?: boolean;
 }
 
@@ -24,7 +24,7 @@ function WidgetWrapper({
       // aria-describedby="DndDescribedBy-0"
       className="relative overflow-hidden rounded-2xl border border-slate-200 bg-white shadow-1 cursor-default w-[416px]"
     >
-      <WidgetButtons widgetItem={widgetItem} />
+      {widgetItem ? <WidgetButtons widgetItem={widgetItem} /> : null}
 
       <div className="border-t border-slate-200">
         {/** 아래 div에 컬러 스키마 클래스네임 추가 */}
