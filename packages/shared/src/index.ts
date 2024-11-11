c04bc9e12629aa9fa06b9f262f399055af528c2c
Eun Oh <bdohhhhh@gmail.com>
2024-11-11 13:18:14 +0900

    [update]: QueryProvider storybook에서도 사용해야하므로 shared로 이동

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,5 +2,6 @@ export * from './apis';
 export * from './components';
 export * from './constants';
 export * from './examples';
+export * from './query';
 export * from './types';
 export * from './utils';
