d227490fc9576bff409964ff60c5f892effb03b4
Eun Oh <bdohhhhh@gmail.com>
2024-11-26 23:49:23 +0900

    [update]: useSlider shared 로 분리

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,6 +3,7 @@ export * from './components';
 export * from './constants';
 export * from './dnd';
 export * from './examples';
+export * from './hooks';
 export * from './query';
 export * from './types';
 export * from './utils';
