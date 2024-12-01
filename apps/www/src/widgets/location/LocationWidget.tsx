e3fd7773294b3d5d9ee5c18c9a567f386a4e9917
Eun Oh <bdohhhhh@gmail.com>
2024-12-01 16:12:08 +0900

    [update]: LocationWidget 에서 주소복사 동작 수정(토스트열기, 주소복사)

--- apps/www/src/widgets/location/LocationWidget.tsx
+++ apps/www/src/widgets/location/LocationWidget.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import type { IInvitation, LocationWidgetConfig, WidgetItem } from '@repo/shared';
-import { Location } from '@repo/shared';
+import { cn, Location } from '@repo/shared';
 
 import { ComponentWithKakaoScript, WidgetWrapper } from '../components';
+import { useToastStore } from '../zustand';
 
 interface LocationWidgetProps {
   widgetItem: WidgetItem;
@@ -12,14 +13,24 @@ interface LocationWidgetProps {
 }
 
 function LocationWidget({ widgetItem, invitation, isMultiModal = false }: LocationWidgetProps) {
+  const { openToast } = useToastStore();
+
+  const handleCopyAddress = async () => {
+    if (!invitation) return;
+    await navigator.clipboard.writeText(invitation.location.address);
+    openToast(`[${invitation.location.address}]가 복사되었습니다.`);
+  };
+
   return (
     <WidgetWrapper widgetItem={widgetItem} isMultiModal={isMultiModal}>
-      <div className="no-interaction">
+      <div className={cn('', isMultiModal ? '' : 'no-interaction')}>
         {invitation ? (
           <ComponentWithKakaoScript>
             <Location
               config={widgetItem.config as LocationWidgetConfig}
               invitationLocation={invitation.location}
+              isMultiModal={isMultiModal}
+              onCopyAddress={handleCopyAddress}
             />
           </ComponentWithKakaoScript>
         ) : null}
