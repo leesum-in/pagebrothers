c410b907686d68ea45a0979af8b5d176929afe51
Eun Oh <bdohhhhh@gmail.com>
2025-02-06 14:52:32 +0900

    [update]: 타이틀, 내용 UI 컴포넌트화

--- apps/www/src/widgets/event-sequence/EventSequenceWidgetConfigure.tsx
+++ apps/www/src/widgets/event-sequence/EventSequenceWidgetConfigure.tsx
@@ -10,13 +10,12 @@ import { LabelWithSub } from '@repo/shared';
 import { useCallback, useEffect } from 'react';
 import type { SubmitHandler } from 'react-hook-form';
 import { useFieldArray, useFormContext } from 'react-hook-form';
-import { FaRegTrashAlt } from 'react-icons/fa';
-import { HiPlus } from 'react-icons/hi2';
 import { useShallow } from 'zustand/shallow';
 
 import { FixedLoader } from '@/www/ui';
 
-import { WidgetBreakLine } from '../components';
+import { WidgetAddListButton, WidgetBreakLine, WidgetLabelWithInput } from '../components';
+import WidgetLiTitleAndDesc from '../components/WidgetLiTitleAndDesc';
 import WidgetThreeWaySelector from '../components/WidgetThreeWaySelector';
 import { useWidgetIndex } from '../hooks';
 import { useInvitationConfigMutation } from '../mutations';
@@ -114,6 +113,15 @@ function EventSequenceWidgetConfigure({ widgetItem }: EventSequenceWidgetConfigu
             addOn={<span className="text-sm text-slate-400">(선택)</span>}
           />
         </div>
+        <div>
+          <WidgetLabelWithInput
+            labelClassName="relative flex items-center overflow-hidden rounded-lg border focus-within:ring border-slate-200"
+            inputClassName="peer block h-12 w-full bg-white px-4 text-slate-600 placeholder:text-slate-300 disabled:cursor-not-allowed disabled:bg-slate-100 disabled:text-slate-200"
+            defaultValue={(widgetItem.config as EventSequenceWidgetConfig).title}
+            register={register}
+            registerOption={`invitation.widgets.${widgetIndex}.config.title`}
+          />
+        </div>
       </div>
 
       <WidgetBreakLine />
@@ -127,54 +135,21 @@ function EventSequenceWidgetConfigure({ widgetItem }: EventSequenceWidgetConfigu
         <div>
           <ul className="space-y-4">
             {(eventSequenceFields as EventSequenceItem[]).map((field, index) => (
-              <li className="relative" key={field.title}>
-                <label className="relative flex items-center overflow-hidden rounded-md border bg-white focus-within:ring border-slate-200 rounded-b-none focus-within:z-10">
-                  <div className="flex flex-none items-center" />
-                  <input
-                    className="peer block h-12 w-full bg-white px-4 text-slate-600 placeholder:text-slate-300 disabled:cursor-not-allowed disabled:bg-slate-100 disabled:text-slate-200 "
-                    spellCheck="false"
-                    autoComplete="off"
-                    placeholder="질문"
-                    {...register(
-                      `invitation.widgets.${widgetIndex}.config.items.${index}.title` as const,
-                    )}
-                  />
-                  <div className="flex flex-none items-center">
-                    <button
-                      tabIndex={-1}
-                      className="absolute bottom-0 right-0 p-4 text-base text-red-500"
-                      type="button"
-                      onClick={() => handleRemoveClick(index)}
-                    >
-                      <FaRegTrashAlt />
-                    </button>
-                  </div>
-                </label>
-                <label className="relative flex items-center overflow-hidden rounded-lg border focus-within:ring border-slate-200 -mt-[1px] rounded-t-none">
-                  <div className="flex items-center" />
-                  <textarea
-                    className="block w-full resize-none bg-white px-4 py-3 text-slate-600 placeholder:text-slate-300 disabled:cursor-not-allowed disabled:bg-slate-100 disabled:text-slate-300 undefined"
-                    spellCheck="false"
-                    autoComplete="off"
-                    rows={3}
-                    placeholder="답변을 입력해주세요"
-                    {...register(
-                      `invitation.widgets.${widgetIndex}.config.items.${index}.description` as const,
-                    )}
-                  />
-                  <div className="flex items-center" />
-                </label>
-              </li>
+              <WidgetLiTitleAndDesc
+                key={field.title}
+                titleRegister={register(
+                  `invitation.widgets.${widgetIndex}.config.items.${index}.title` as keyof HookFormValues,
+                )}
+                descRegister={register(
+                  `invitation.widgets.${widgetIndex}.config.items.${index}.description` as keyof HookFormValues,
+                )}
+                widgetIndex={widgetIndex}
+                index={index}
+                handleRemoveClick={handleRemoveClick}
+              />
             ))}
             <li>
-              <button
-                type="button"
-                className="w-full h-12 rounded-md px-4 text-sm border border-dashed border-slate-300 center-flex gap-2 font-bold shadow-1 transition-colors disabled:opacity-40"
-                onClick={handleAddClick}
-              >
-                <span>구성 추가하기</span>
-                <HiPlus />
-              </button>
+              <WidgetAddListButton handleAddClick={handleAddClick} />
             </li>
           </ul>
         </div>
