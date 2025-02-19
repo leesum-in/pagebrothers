dbcc7bd634db2305874a4f1370cbcab1d510b633
Jiyoung <jiyoung990602@gmail.com>
2025-02-19 19:25:42 +0900

    update : Props invitation 추가

--- apps/www/src/widgets/guestbook/GuestbookWidget.tsx
+++ apps/www/src/widgets/guestbook/GuestbookWidget.tsx
@@ -1,19 +1,21 @@
 import Guestbook from '@repo/shared/src/components/widgets/guestbook/Guestbook';
 import { WidgetWrapper } from '../components';
-import type { WidgetItem } from '@repo/shared';
+import type { IInvitation, WidgetItem } from '@repo/shared';
 
 interface GuestbookWidgetProps {
-  widgetItem?: WidgetItem;
+  invitation?: IInvitation;
+  widgetItem: WidgetItem;
   isMultiModal?: boolean;
 }
 
 function GuestbookWidget({
   widgetItem,
   isMultiModal = false,
+  invitation,
 }: GuestbookWidgetProps): React.ReactNode {
   return (
     <WidgetWrapper widgetItem={widgetItem} isMultiModal={isMultiModal}>
-      <Guestbook widgetItem={widgetItem} isMultiModal={isMultiModal} />
+      {invitation ? <Guestbook widgetItem={widgetItem} isMultiModal={isMultiModal} /> : null}
     </WidgetWrapper>
   );
 }
